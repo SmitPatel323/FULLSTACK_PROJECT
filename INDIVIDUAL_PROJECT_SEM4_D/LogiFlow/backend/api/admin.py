6bebf656c0bbd7c76c7930181418a00f29c00fe2
Patel Smit Vishalkumar <smitpatel0405@gmail.com>
2025-08-24 10:31:26 +0530

    Delete INDIVIDUAL_PROJECT_SEM4_D directory

--- INDIVIDUAL_PROJECT_SEM4_D/LogiFlow/backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Product, Vehicle, DeliveryAgent, Shipment
-
-admin.site.register(User)
-admin.site.register(Product)
-admin.site.register(Vehicle)
-admin.site.register(DeliveryAgent)
-admin.site.register(Shipment)
